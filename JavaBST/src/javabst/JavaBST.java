f9bb6e4f7af0e1239c7124eef6ff489d46d78cf2
Kieran Chang <kjc882@mail.missouri.edu>
2018-04-17 19:23:13 -0500

    Ready for Testing
    In classic java fashion i accidently created an instance variable instead of my class wide variable so i would only output zeroes. But this one appears to be working

--- JavaBST/src/javabst/JavaBST.java
+++ JavaBST/src/javabst/JavaBST.java
@@ -1,7 +1,6 @@
 /*
  * Kieran Chang 12446452
  * University of Columbia Computer Science
- * CS3330 Object Oriented Programming SP18
  */
 package javabst;
 
@@ -11,16 +10,68 @@ package javabst;
  */
 public class JavaBST {
     
+    //Node Class
+    //Nodes may only take ints in this example
+    class Node{
+        int key;
+        Node left, right;
+        
+        //Constructor
+        public Node(int num){
+            key = num;
+            left = right = null;
+        }
+    }
+    
     Node root;
     
     //JavaBST constructor
-    JavaBST(int key){
-        root = new Node(key);
-    }
-    
     JavaBST(){
         root = null;
     }
+    
+    public void inOrder(){
+        inOrderReal(root);
+    }
+    
+    //traverse the tree in order
+    public void inOrderReal(Node root){
+        if(root != null){
+            inOrderReal(root.left);
+            System.out.println(root.key);
+            inOrderReal(root.right);
+        }
+    }
+    
+    public void insertSet(int key){
+        root = insertReal(root, key);
+    }
+    
+    public Node insertReal(Node root, int key){
+        
+        //base case
+        if(root == null){
+            System.out.println("insertion");
+            root = new Node(key);
+            return root;
+        }
+        
+        //if this key is smaller than the current
+        //root's key move left
+        if(key < root.key){
+            System.out.println("left");
+            System.out.println(root.key);
+            root.left = insertReal(root.left, key);
+        }
+        //else move right
+        else if(key > root.key){
+            System.out.println("right");
+            System.out.println(root.key);
+            root.right = insertReal(root.right, key);
+        }
+        return root;
+    }
+    
     public static void main(String[] args) {
         /*   BST
               50
@@ -31,60 +82,16 @@ public class JavaBST {
         */
        
         //creating the tree
-        JavaBST bst = new JavaBST(50);
-        bst.insert(bst.root, 30);
-        bst.insert(bst.root, 20);
-        bst.insert(bst.root, 40);
-        bst.insert(bst.root, 70);
-        bst.insert(bst.root, 60);
-        bst.insert(bst.root, 80);
+        JavaBST bst = new JavaBST();
+        bst.insertSet(50);
+        bst.insertSet(30);
+        bst.insertSet(20);
+        bst.insertSet(40);
+        bst.insertSet(70);
+        bst.insertSet(60);
+        bst.insertSet(80);
         
         //display the tree in order
-        bst.inOrder(bst.root);
-    }
-    
-    //Node Class
-    //Nodes may only take ints in this example
-    class Node{
-        int key;
-        Node left, right;
-        
-        //Constructor
-        public Node(int num){
-            int key = num;
-            left = right = null;
-        }
-    }
-    
-    //traverse the tree in order
-    public void inOrder(Node root){
-        if(root != null){
-            inOrder(root.left);
-            System.out.println(root.key);
-            inOrder(root.right);
-        }
-    }
-    
-    public Node insert(Node node, int key){
-        
-        //if the tree is empty on insert
-        if(node == null){
-            System.out.println("insertion");
-            Node node1 = new Node(key);
-            return node1;
-        }
-        
-        //if this key is smaller than the current
-        //node's key move left
-        if(key < node.key){
-            System.out.println("left");
-            node.left = insert(node.left, key);
-        }
-        //else move right
-        else{
-            System.out.println("right");
-            node.right = insert(node.right, key);
-        }
-        return node;
+        bst.inOrder();
     }
 }
