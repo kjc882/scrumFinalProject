da92ad68a73a753b7a75a2f3eb157f0b578266bc
Kieran Chang <kjc882@mail.missouri.edu>
2018-04-19 18:33:21 -0500

    First Commit
    Starting Queue

---
+++ JavaQueue/src/javaqueue/JavaQueue.java
@@ -0,0 +1,21 @@
+/*
+ * Kieran Chang 12446452
+ * University of Columbia Computer Science
+ * CS3330 Object Oriented Programming SP18
+ */
+package javaqueue;
+
+/**
+ *
+ * @author Kieran
+ */
+public class JavaQueue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
