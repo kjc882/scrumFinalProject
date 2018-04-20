49e42fb203d37daa5dd1331fa466d4ba48771a82
Kieran Chang <kjc882@mail.missouri.edu>
2018-04-19 19:56:56 -0500

    Ready for testing
    This bubble sort is ready for testing. it appears to work on my end

--- JavaBubbleSort/src/javabubblesort/JavaBubbleSort.java
+++ JavaBubbleSort/src/javabubblesort/JavaBubbleSort.java
@@ -11,11 +11,43 @@ package javabubblesort;
  */
 public class JavaBubbleSort {
 
+    //the entire sorting algorithm
+    public void bubbleSort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+    
+    public void printArray(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JavaBubbleSort jbs = new JavaBubbleSort();
+
+        //create the array
+        int[] array = {50, 30, 20, 40, 70, 60, 80};
+        
+        //print the array before sorting
+        System.out.println("Before Sorting");
+        jbs.printArray(array);
+        jbs.bubbleSort(array);
+        
+        //print the array after sorting
+        System.out.println("After Sorting");
+        jbs.printArray(array);
     }
     
 }
